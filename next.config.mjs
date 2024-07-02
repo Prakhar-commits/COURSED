01258bcc2303d212d6f4a88285deb16e38afbcc0
Prakhar <prakharb56@gmail.com>
2024-07-02 09:28:27 +0530

    updated nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com; object-src 'none'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self' https://api.example.com; frame-src 'self' https://accounts.google.com;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
